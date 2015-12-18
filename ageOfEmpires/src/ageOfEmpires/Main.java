42570a8b3a6f8dda3497a8422a6a02c34a33e6af
rmeek <rmeek@HC-AD-33270.halesowen.ac.uk>
2015-12-18 14:59:11 +0000

    1

--- ageOfEmpires/src/ageOfEmpires/Main.java
+++ ageOfEmpires/src/ageOfEmpires/Main.java
@@ -1,9 +1,71 @@
 package ageOfEmpires;
 
-public class Main {
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame{
+	
+	private static final long serialVersionUID = 1L;
+	static boolean running = false;
+	static int width,height;
+	
+	private static GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0];
+
 	
 	public static void main(String[] args) {
+		Main main = new Main();
+		main.run();
+	}
+	
+	private void run(){
+		initialise();
+		while(running){
+			long beforeTime = System.currentTimeMillis();
+			update();
+			draw();
+			long afterTime = System.currentTimeMillis();
+			long diff = afterTime - beforeTime;
+			long waitTime = diff / 45;
+			try{
+				if(waitTime > 0){
+					Thread.sleep(waitTime);
+				}
+			}catch (Exception e){
+				e.printStackTrace();
+			}
+		}
+		dispose();
 		
+	}
+	
+	private void initialise(){
+		running = true;
+		setTitle("Age of Empires");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		width = Toolkit.getDefaultToolkit().getScreenSize().width;
+		height = Toolkit.getDefaultToolkit().getScreenSize().height;
+		setSize(width, height);
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		setUndecorated(true);
+		device.setFullScreenWindow(this);
+		setVisible(running);
+		setResizable(false);
+		
+	}
+	
+	private void update(){
+		
+		
+		
+	}
+
+	private void draw(){
+	
+	
+	
 	}
 	
 }
---
+++ ageOfEmpires/src/ageOfEmpires/Map.java
@@ -0,0 +1,17 @@
+package ageOfEmpires;
+
+public class Map {
+
+	private terrain[][] map;
+	
+	private enum terrain{
+		GRASS,WOODLAND
+	}
+	
+	public Map(){
+		
+		
+		
+	}
+	
+}
