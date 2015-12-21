8433cf96d224572d3d2bdb61f99218a7c023bd6e
Oblivion-nara <reecemeek@gmail.com>
2015-12-21 23:44:04 +0000

    new stuff

--- ageOfEmpires/src/ageOfEmpires/Main.java
+++ ageOfEmpires/src/ageOfEmpires/Main.java
@@ -1,17 +1,31 @@
 package ageOfEmpires;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.util.Random;
 
 import javax.swing.JFrame;
 
 public class Main extends JFrame{
 	
 	private static final long serialVersionUID = 1L;
+	public static int width,height;
+	public static double zeroXCoord,zeroYCoord;
 	static boolean running = false;
-	static int width,height;
 	
+	private long updatetime;
+	
+	static final double root2 = Math.sqrt(2);
+	static Random gen = new Random();
+	
+	private InputHandler input;
+	private Map map;
 	private static GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0];
 
 	
@@ -48,24 +62,95 @@ public class Main extends JFrame{
 		width = Toolkit.getDefaultToolkit().getScreenSize().width;
 		height = Toolkit.getDefaultToolkit().getScreenSize().height;
 		setSize(width, height);
+		System.out.println(width+","+height);
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
 		setUndecorated(true);
 		device.setFullScreenWindow(this);
 		setVisible(running);
 		setResizable(false);
+		input = new InputHandler(this);
+		map = new Map();
+		zeroXCoord = 0;
+		zeroYCoord = 0;
+		updatetime = System.currentTimeMillis() + 10;
 		
+	}
+	
+	public void moveUp(){
+		zeroYCoord += 1;
+	}
+	public void moveDown(){
+		zeroYCoord -= 1;
+	}
+	public void moveRight(){
+		zeroXCoord -= 1;
+	}
+	public void moveLeft(){
+		zeroXCoord += 1;
+	}
+	public void moveUR(){
+		zeroYCoord += 1/1/root2;
+		zeroXCoord -= 1/root2;
+	}
+	public void moveUL(){
+		zeroYCoord += 1/root2;
+		zeroXCoord += 1/root2;
+	}
+	public void moveDR(){
+		zeroYCoord -= 1/root2;
+		zeroXCoord -= 1/root2;
+	}
+	public void moveDL(){
+		zeroYCoord -= 1/root2;
+		zeroXCoord += 1/root2;
 	}
 	
 	private void update(){
 		
-		
+		if(updatetime <= System.currentTimeMillis()){
+			
+			if(input.isKeyDown(KeyEvent.VK_W)){
+				if(input.isKeyDown(KeyEvent.VK_D)){
+					moveUR();
+				}else if(input.isKeyDown(KeyEvent.VK_A)){
+					moveUL();
+				}else{
+					moveUp();
+				}
+			}else if(input.isKeyDown(KeyEvent.VK_S)){
+				if(input.isKeyDown(KeyEvent.VK_D)){
+					moveDR();
+				}else if(input.isKeyDown(KeyEvent.VK_A)){
+					moveDL();
+				}else{
+					moveDown();
+				}
+			}else if(input.isKeyDown(KeyEvent.VK_D)){
+				moveRight();
+			}else if(input.isKeyDown(KeyEvent.VK_A)){
+				moveLeft();
+			}else
+			
+			updatetime = System.currentTimeMillis() + 10;
+			
+		}
 		
 	}
 
 	private void draw(){
-	
-	
-	
+
+		Graphics g = getGraphics();
+		Graphics2D g2d = (Graphics2D) g;
+		Image offImage = createImage(map.getXSize(), map.getYSize());
+		Graphics offGraphics = offImage.getGraphics();
+		offGraphics.setColor(Color.BLUE);
+		offGraphics.fillRect((int)-zeroXCoord, (int)-zeroYCoord, width, height);
+		offGraphics.setColor(Color.orange);
+		offGraphics.drawRect(0, 0, width+1, height+1);
+		offGraphics.setColor(Color.black);
+		offGraphics.fillRect(width/2-10, height/2-10, 20, 20);
+		g2d.drawImage(offImage, (int)zeroXCoord, (int)zeroYCoord, width, height, null);
+		
 	}
 	
 }
--- ageOfEmpires/src/ageOfEmpires/Map.java
+++ ageOfEmpires/src/ageOfEmpires/Map.java
@@ -3,6 +3,7 @@ package ageOfEmpires;
 public class Map {
 
 	private terrain[][] map;
+	private int xSize,ySize;
 	
 	private enum terrain{
 		GRASS,WOODLAND
@@ -10,8 +11,27 @@ public class Map {
 	
 	public Map(){
 		
+		int map = Main.gen.nextInt(5);
+		switch (map){
+		case 0:
+			break;
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		}
 		
-		
+	}
+	
+	public int getXSize(){
+		return xSize;
+	}
+	public int getYSize(){
+		return ySize;
 	}
 	
 }
