900f4ce9d62afa66c2d197b222b49378b09a5d12
Oblivion-nara <reecemeek@gmail.com>
2016-01-04 19:15:24 +0000

    stuff

---
+++ ageOfEmpires/src/ageOfEmpires/PlayerBuildings.java
@@ -0,0 +1,16 @@
+package ageOfEmpires;
+
+public class PlayerBuildings {
+
+	private final String townHall = "";
+	private final String barracks = "";
+	private final String house = "";
+	
+	
+	public PlayerBuildings() {
+		
+		
+		
+	}
+
+}
