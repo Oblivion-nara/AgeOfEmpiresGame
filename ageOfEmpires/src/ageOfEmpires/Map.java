a8e46f012d22865f99292d8fac603d50c11ca8db
mxw596 <mxw596@ISLC-UG08-19.adf.bham.ac.uk>
2016-01-20 15:20:25 +0000

    merge changes added noise map for the game

--- ageOfEmpires/src/ageOfEmpires/Map.java
+++ ageOfEmpires/src/ageOfEmpires/Map.java
@@ -6,6 +6,8 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Point;
 
+import utils.NoiseGenerator;
+import utils.MathHelper;
 import utils.ResourceLoader;
 
 public class Map{
@@ -13,6 +15,7 @@ public class Map{
 	private Point[][] mapPics;   //which mapSprite to use, as a point
 	private terrain[][] map;
 	private Image[][] mapSprites;
+	private double[][] noise;
 	private int xSize,ySize;
 	
 	private enum terrain{
@@ -29,20 +32,20 @@ public class Map{
 		
 		switch (mapsize){
 		case 1:
-			ySize = 1080;
-			xSize = 1760;
+			xSize = MathHelper.ceiling(Main.width,160)*160;
+			ySize = MathHelper.ceiling(Main.height,160)*160;
 			break;
 		case 2:
-			ySize = 1440;
-			xSize = 2080;
+			xSize = MathHelper.ceiling(3*MathHelper.ceiling(Main.width,2),160)*160;
+			xSize = MathHelper.ceiling(3*MathHelper.ceiling(Main.height,2),160)*160;
 			break;
 		case 3:
-			ySize = 1760;
-			xSize = 2400;
+			xSize = MathHelper.ceiling(2*Main.width,160)*160;
+			ySize = MathHelper.ceiling(2*Main.height,160)*160;
 			break;
 		}
 		
-		map = new terrain[xSize/10][ySize/10];
+		/*map = new terrain[xSize/10][ySize/10];
 		mapPics = new Point[xSize][ySize];
 		
 		mapSprites = ResourceLoader.getPlayerSprites("background/landSprites.png", 16, 16);
@@ -52,7 +55,10 @@ public class Map{
 			for (int j = 0; j < mapPics[0].length; j++) {
 				mapPics[i][j] = new Point(0,Main.gen.nextInt(4));
 			}
-		}
+		}*/
+		
+		NoiseGenerator noiseGen = new NoiseGenerator(Main.gen.nextLong());
+		noise = noiseGen.getPerlinNoise(xSize, ySize, 4, 5);
 		
 	}
 	
@@ -68,12 +74,12 @@ public class Map{
 	public void draw(Graphics g){
 		
 		Graphics2D g2 = (Graphics2D)g;
-//		for (int i = 0; i < mapPics.length; i++) {
-//			for (int j = 0; j < mapPics[0].length; j++) {
-//					g2.drawImage(mapSprites[mapPics[i][j].x][mapPics[i][j].y], i*16, j*16, null);
-//				
-//			}
-//		}
+		/*for (int i = 0; i < mapPics.length; i++) {
+			for (int j = 0; j < mapPics[0].length; j++) {
+					g2.drawImage(mapSprites[mapPics[i][j].x][mapPics[i][j].y], i*16, j*16, null);
+				
+			}
+		}
 		g2.setColor(Color.blue);
 		int y = 0;
 		int width = 10;
@@ -94,7 +100,21 @@ public class Map{
 				g2.drawLine(x-width, i, x+width, i);
 			}
 			
-		}while(y <= ySize);
+		}while(y <= ySize);*/
+		for(int i = 0; i < noise.length; i++){
+			for(int j = 0; j < noise[0].length; j++){
+				if(noise[i][j] > 0.4 && noise[i][j] < 0.5){
+					g2.setColor(Color.blue);
+				}else if(noise[i][j] > 0.95){
+					g2.setColor(Color.white);
+				}else if(noise[i][j] > 0.7){
+					g2.setColor(Color.gray);
+				}else{
+					g2.setColor(Color.green);
+				}
+				g2.fillRect(i*16, j*16, 16, 16);;
+			}
+		}
 		
 	}
 }
